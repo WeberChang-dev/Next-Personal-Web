21effb48e62601e07627e2a4102c374ac3c21e81
Weber Chang <s0975247623@gmail.com>
2025-12-21 16:33:24 +0800

    Fix image file name

--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -6,7 +6,7 @@ const items = [{
   title: "Associate Software Developer Intern @ Google Taiwan",
   date: "Summer 2025",
   subtitle: "Optimized Pixel Engineering Testing workflow with test triggering and result propagating.",
-  imageUrl: '/static/images/STEP.jpg'
+  imageUrl: '/static/images/step.jpg'
 }, {
   title: "Game Dev at 2024 NTU CSIE Camp Challenge",
   date: "Summer 2024",
