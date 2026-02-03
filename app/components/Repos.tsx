4ec422ca45ab2c4670d0cf5ac1f6b3fee38d3c92
Weber Chang <s0975247623@gmail.com>
2026-02-03 21:21:45 +0800

    Remove cache

--- app/components/Repos.tsx
+++ app/components/Repos.tsx
@@ -12,9 +12,7 @@ interface Repo  {
 }
 
 const getRepos = async () => {
-    const res = await fetch('https://gh-pinned-repos-tsj7ta5xfhep.deno.dev/?username=WeberChang-dev', {
-        next: { revalidate: 60 * 60 * 24 * 30 } // a month
-    })
+    const res = await fetch('https://gh-pinned-repos-tsj7ta5xfhep.deno.dev/?username=WeberChang-dev')
     const repos = await res.json()
     
     return repos
